30801eb0c99dd2a00cf62d43e7667c75bfd089f5
israel <israel@comunidad.unam.mx>
2025-03-25 12:18:42 -0600

    Version 0.0.1

---
+++ src/main/java/com/losung/projectmanager/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.losung.projectmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Project Manager");
+        model.addAttribute("message", "Bienvenido al Sistema de Gestión de Proyectos");
+        return "home";
+    }
+} 
